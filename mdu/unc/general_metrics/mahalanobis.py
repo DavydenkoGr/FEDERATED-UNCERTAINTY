Mahalanobis for ensembles fixed -- for each model we fit individual parameters, and then we average resulting distances

mdu/unc/general_metrics/mahalanobis.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -11,8 +12,9 @@ class MahalanobisDistance(BaseEstimator, TransformerMixin):
 
     def __init__(self, regularization: float = 1e-6):
         self.regularization = regularization
-        self.mean_ = None
-        self.inv_cov_ = None
+        self.mean_ = defaultdict(lambda: None)
+        self.inv_cov_ = defaultdict(lambda: None)
+        self.n_models_ = None
 
     def fit(self, X: np.ndarray, y=None):
         """
@@ -24,10 +26,12 @@ class MahalanobisDistance(BaseEstimator, TransformerMixin):
         Returns:
             self
         """
-        self.mean_ = np.mean(X, axis=0)
-        cov = np.cov(X, rowvar=False)
-        cov += np.eye(cov.shape[0]) * self.regularization
-        self.inv_cov_ = np.linalg.inv(cov)
+        self.n_models_ = X.shape[0]
+        for i in range(self.n_models_):
+            self.mean_[i] = np.mean(X[i], axis=0)
+            cov = np.cov(X[i], rowvar=False)
+            cov += np.eye(cov.shape[0]) * self.regularization
+            self.inv_cov_[i] = np.linalg.inv(cov)
         return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
@@ -39,11 +43,13 @@ class MahalanobisDistance(BaseEstimator, TransformerMixin):
         Returns:
             distances: np.ndarray of shape (n_samples,)
         """
-        if self.mean_ is None or self.inv_cov_ is None:
+        if self.mean_[0] is None or self.inv_cov_[0] is None:
             raise RuntimeError(
                 "MahalanobisUncertainty must be fitted before calling predict."
             )
-        diff = X - self.mean_
-        left = np.dot(diff, self.inv_cov_)
-        distances = np.sqrt(np.sum(left * diff, axis=1))
-        return distances
+        distances = []
+        for i in range(self.n_models_):
+            diff = X[i] - self.mean_[i]
+            left = np.dot(diff, self.inv_cov_[i])
+            distances.append(np.sqrt(np.sum(left * diff, axis=1)))
+        return np.mean(distances, axis=0)
9aa390b1ae9138ad70e2cfe8468daaa5cba221fb
Nikita Kotelevskii <niket0996@gmail.com>
2025-09-01 19:29:42 +0300

    refactor(experiments): misclassification and ood detection files has been changed, so that is incapsulates all the machinery within functions

--- main_images_mis.py
+++ main_images_mis.py
@@ -13,164 +13,183 @@ import pandas as pd
 from configs.uncertainty_measures_configs import (
     MAHALANOBIS_AND_BAYES_RISK,
     EXCESSES_DIFFERENT_INSTANTIATIONS,
-    EXCESSES_DIFFERENT_APPROXIMATIONS,
-    BAYES_DIFFERENT_APPROXIMATIONS,
+    EXCESSES_DIFFERENT_APPROXIMATIONS_LOGSCORE,
+    BAYES_DIFFERENT_APPROXIMATIONS_LOGSCORE,
+    BAYES_DIFFERENT_APPROXIMATIONS_SPHERICALSCORE,
     BAYES_DIFFERENT_INSTANTIATIONS,
 )
+from mdu.randomness import set_all_seeds
 
-UNCERTAINTY_MEASURES = BAYES_DIFFERENT_INSTANTIATIONS
+def main(ensemble_groups, train_dataset, eval_dataset, uncertainty_measures, multidim_model, multidim_params, train_kwargs, weights_root, seed):
 
-MULTIDIM_MODEL = VectorQuantileModel.ENTROPIC_OT
+    set_all_seeds(seed)
 
-device = torch.device("cuda:0")
+    results = defaultdict(list)
 
-if MULTIDIM_MODEL == VectorQuantileModel.CPFLOW:
-    train_kwargs = {
-        "lr": 1e-4,
-        "num_epochs": 10,
-        "batch_size": 64,
-        "device": device,
-    }
-    multidim_params = {
-        "feature_dimension": len(UNCERTAINTY_MEASURES),
-        "hidden_dim": 8,
-        "num_hidden_layers": 5,
-        "nblocks": 4,
-        "zero_softplus": False,
-        "softplus_type": "softplus",
-        "symm_act_first": False,
-    }
+    for group in ensemble_groups:
+        all_ind_logits = []
+        for model_id in group:
+            eval_res = load_pickle(
+                f"{weights_root}/{train_dataset}/checkpoints/resnet18/CrossEntropy/{model_id}/{eval_dataset}.pkl"
+            )
 
-elif MULTIDIM_MODEL == VectorQuantileModel.OTCP:
-    train_kwargs = {
-        "batch_size": 64,
-        "device": device,
-    }
-    multidim_params = {
-        "positive": True,
-    }
-elif MULTIDIM_MODEL == VectorQuantileModel.ENTROPIC_OT:
-    train_kwargs = {
-        "batch_size": 64,
-        "device": device,
-    }
-    multidim_params = {
-        "target": "exp",
-        "standardize": True,
-        "fit_mse_params": False,
-        "eps": 0.15,
-    }
-else:
-    raise ValueError(f"Invalid multidim model: {MULTIDIM_MODEL}")
+            logits_ind = eval_res["embeddings"]
+            all_ind_logits.append(eval_res["embeddings"][None])
 
+        y_true = eval_res["labels"]
 
-ENSEMBLE_GROUPS = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-]
-
-train_dataset = DatasetName.CIFAR10_NOISY.value
-eval_dataset = DatasetName.CIFAR10.value
-
-results = defaultdict(list)
-
-for group in ENSEMBLE_GROUPS:
-    all_ind_logits = []
-    for model_id in group:
-        eval_res = load_pickle(
-            f"./model_weights/{train_dataset}/checkpoints/resnet18/CrossEntropy/{model_id}/{eval_dataset}.pkl"
+        _, train_cond_idx, calib_idx, test_idx = split_dataset_indices(
+            logits_ind,
+            y_true,
+            train_ratio=0.0,
+            calib_ratio=0.1,
+            test_ratio=0.8,
         )
 
-        logits_ind = eval_res["embeddings"]
-        all_ind_logits.append(eval_res["embeddings"][None])
+        y_train_cond = y_true[train_cond_idx]
+        y_calib = y_true[calib_idx]
+        y_test = y_true[test_idx]
 
-    y_true = eval_res["labels"]
+        X_train_cond = np.vstack(all_ind_logits)[:, train_cond_idx, :]
+        X_calib = np.vstack(all_ind_logits)[:, calib_idx, :]
+        X_test = np.vstack(all_ind_logits)[:, test_idx, :]
 
-    _, train_cond_idx, calib_idx, test_idx = split_dataset_indices(
-        logits_ind,
-        y_true,
-        train_ratio=0.0,
-        calib_ratio=0.1,
-        test_ratio=0.8,
-    )
+        y_pred = np.argmax(np.mean(X_test, axis=0), axis=-1)
 
-    y_train_cond = y_true[train_cond_idx]
-    y_calib = y_true[calib_idx]
-    y_test = y_true[test_idx]
+        print(f"Ensemble accuracy: {np.mean(y_pred == y_test)}")
 
-    X_train_cond = np.vstack(all_ind_logits)[:, train_cond_idx, :]
-    X_calib = np.vstack(all_ind_logits)[:, calib_idx, :]
-    X_test = np.vstack(all_ind_logits)[:, test_idx, :]
-
-    y_pred = np.argmax(np.mean(X_test, axis=0), axis=-1)
-
-    print(f"Ensemble accuracy: {np.mean(y_pred == y_test)}")
-
-    multi_dim_uncertainty = MultiDimensionalUncertainty(
-        UNCERTAINTY_MEASURES,
-        multidim_model=MULTIDIM_MODEL,
-        multidim_params=multidim_params,
-    )
-    multi_dim_uncertainty.fit(
-        logits_train=X_train_cond,
-        y_train=y_train_cond,
-        logits_calib=X_calib,
-        train_kwargs=train_kwargs,
-    )
-
-    _, uncertainty_scores = multi_dim_uncertainty.predict(X_test)
-
-    # Compute ROC AUC between in-distribution (class 0) and OOD (class 1) using sklearn
-    for k in uncertainty_scores.keys():
-        correct_scores = uncertainty_scores[k][y_test == y_pred]
-        incorrect_scores = uncertainty_scores[k][y_test != y_pred]
-
-        # Concatenate scores and labels
-        all_scores = np.concatenate([correct_scores, incorrect_scores])
-        all_labels = np.concatenate(
-            [
-                np.zeros_like(correct_scores),  # class 0: correct scores
-                np.ones_like(incorrect_scores),  # class 1: incorrect score
-            ]
+        multi_dim_uncertainty = MultiDimensionalUncertainty(
+            uncertainty_measures,
+            multidim_model=multidim_model,
+            multidim_params=multidim_params,
+            if_add_maximal_elements=True,
+        )
+        multi_dim_uncertainty.fit(
+            logits_train=X_train_cond,
+            y_train=y_train_cond,
+            logits_calib=X_calib,
+            train_kwargs=train_kwargs,
         )
 
-        auc = roc_auc_score(all_labels, all_scores)
-        results[k].append(auc)
+        _, uncertainty_scores = multi_dim_uncertainty.predict(X_test)
 
-print(f"train_cond_idx.shape: {train_cond_idx.shape}")
-print(f"calib_idx.shape: {calib_idx.shape}")
-print(f"test_idx.shape: {test_idx.shape}")
+        # Compute ROC AUC between in-distribution (class 0) and OOD (class 1) using sklearn
+        for k in uncertainty_scores.keys():
+            correct_scores = uncertainty_scores[k][y_test == y_pred]
+            incorrect_scores = uncertainty_scores[k][y_test != y_pred]
 
-print(f"For metrics: {[m['print_name'] for m in UNCERTAINTY_MEASURES]}")
+            # Concatenate scores and labels
+            all_scores = np.concatenate([correct_scores, incorrect_scores])
+            all_labels = np.concatenate(
+                [
+                    np.zeros_like(correct_scores),  # class 0: correct scores
+                    np.ones_like(incorrect_scores),  # class 1: incorrect score
+                ]
+            )
+
+            auc = roc_auc_score(all_labels, all_scores)
+            results[k].append(auc)
+
+    print(f"train_cond_idx.shape: {train_cond_idx.shape}")
+    print(f"calib_idx.shape: {calib_idx.shape}")
+    print(f"test_idx.shape: {test_idx.shape}")
+
+    print(f"For metrics: {[m['print_name'] for m in uncertainty_measures]}")
 
 
-rows = []
-for metric, aucs in results.items():
-    mean_auc = np.mean(aucs)
-    std_auc = np.std(aucs)
-    row = {
-        "Train dataset": train_dataset,
-        "Eval dataset": eval_dataset,
-        "Metric": metric,
-        "ROC AUC Scores": aucs,
-        "Mean ROC AUC": mean_auc,
-        "Std ROC AUC": std_auc,
-    }
-    rows.append(row)
+    rows = []
+    for metric, aucs in results.items():
+        mean_auc = np.mean(aucs)
+        std_auc = np.std(aucs)
+        row = {
+            "Train dataset": train_dataset,
+            "Eval dataset": eval_dataset,
+            "Metric": metric,
+            "ROC AUC Scores": aucs,
+            "Mean ROC AUC": mean_auc,
+            "Std ROC AUC": std_auc,
+        }
+        rows.append(row)
 
-df = pd.DataFrame(
-    rows,
-    columns=[
-        "Train dataset",
-        "Eval dataset",
-        "Metric",
-        "ROC AUC Scores",
-        "Mean ROC AUC",
-        "Std ROC AUC",
-    ],
-)
+    df = pd.DataFrame(
+        rows,
+        columns=[
+            "Train dataset",
+            "Eval dataset",
+            "Metric",
+            "ROC AUC Scores",
+            "Mean ROC AUC",
+            "Std ROC AUC",
+        ],
+    )
 
-print("\nSummary of ROC AUCs across groups:")
-print(df)
+    return df
+
+
+
+
+if __name__ == "__main__":
+    seed = 42
+    UNCERTAINTY_MEASURES = EXCESSES_DIFFERENT_APPROXIMATIONS_LOGSCORE
+    MULTIDIM_MODEL = VectorQuantileModel.ENTROPIC_OT
+    device = torch.device("cuda:0")
+
+    if MULTIDIM_MODEL == VectorQuantileModel.CPFLOW:
+        train_kwargs = {
+            "lr": 1e-4,
+            "num_epochs": 10,
+            "batch_size": 64,
+            "device": device,
+        }
+        multidim_params = {
+            "feature_dimension": len(UNCERTAINTY_MEASURES),
+            "hidden_dim": 8,
+            "num_hidden_layers": 5,
+            "nblocks": 4,
+            "zero_softplus": False,
+            "softplus_type": "softplus",
+            "symm_act_first": False,
+        }
+
+    elif MULTIDIM_MODEL == VectorQuantileModel.OTCP:
+        train_kwargs = {
+            "batch_size": 64,
+            "device": device,
+        }
+        multidim_params = {
+            "positive": True,
+        }
+    elif MULTIDIM_MODEL == VectorQuantileModel.ENTROPIC_OT:
+        train_kwargs = {
+            "batch_size": 64,
+            "device": device,
+        }
+        multidim_params = {
+            "target": "exp",
+            "standardize": False,
+            "fit_mse_params": False,
+            "eps": 0.1,
+            "max_iters": 100,
+            "tol": 1e-6,
+            "random_state": seed,
+        }
+    else:
+        raise ValueError(f"Invalid multidim model: {MULTIDIM_MODEL}")
+
+
+    ENSEMBLE_GROUPS = [
+        [0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9],
+        [10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19],
+    ]
+
+    train_dataset = DatasetName.CIFAR10.value
+    eval_dataset = DatasetName.CIFAR10.value
+    weights_root = "./model_weights"
+
+
+    df = main(ENSEMBLE_GROUPS, train_dataset, eval_dataset, UNCERTAINTY_MEASURES, MULTIDIM_MODEL, multidim_params, train_kwargs, weights_root, seed)
+
+    print("\nSummary of ROC AUCs across groups:")
+    print(df)
--- main_images_ood.py
+++ main_images_ood.py
@@ -11,183 +11,193 @@ import pandas as pd
 from configs.uncertainty_measures_configs import (
     MAHALANOBIS_AND_BAYES_RISK,
     EXCESSES_DIFFERENT_INSTANTIATIONS,
-    EXCESSES_DIFFERENT_APPROXIMATIONS,
-    BAYES_DIFFERENT_APPROXIMATIONS,
+    EXCESSES_DIFFERENT_APPROXIMATIONS_LOGSCORE,
+    EXCESSES_DIFFERENT_APPROXIMATIONS_BRIERSCORE,
+    EXCESSES_DIFFERENT_APPROXIMATIONS_SPHERICALSCORE,
+    BAYES_DIFFERENT_APPROXIMATIONS_LOGSCORE,
+    BAYES_DIFFERENT_APPROXIMATIONS_SPHERICALSCORE,
     BAYES_DIFFERENT_INSTANTIATIONS,
     BAYES_RISK_AND_BAYES_RISK,
     SINGLE_MEASURE,
+    ADDITIVE_TOTALS,
+    CORRESPONDING_COMPONENTS_TO_ADDITIVE_TOTALS,
 )
 from mdu.randomness import set_all_seeds
-seed = 42
-set_all_seeds(seed)
-
-UNCERTAINTY_MEASURES = MAHALANOBIS_AND_BAYES_RISK # + BAYES_RISK_AND_BAYES_RISK + EXCESSES_DIFFERENT_INSTANTIATIONS
-
-# MULTIDIM_MODEL = VectorQuantileModel.CPFLOW
-# MULTIDIM_MODEL = VectorQuantileModel.OTCP
-MULTIDIM_MODEL = VectorQuantileModel.ENTROPIC_OT
-
-device = torch.device("cuda:0")
-
-if MULTIDIM_MODEL == VectorQuantileModel.CPFLOW:
-    train_kwargs = {
-        "lr": 1e-4,
-        "num_epochs": 10,
-        "batch_size": 64,
-        "device": device,
-    }
-    multidim_params = {
-        "feature_dimension": len(UNCERTAINTY_MEASURES),
-        "hidden_dim": 8,
-        "num_hidden_layers": 5,
-        "nblocks": 4,
-        "zero_softplus": False,
-        "softplus_type": "softplus",
-        "symm_act_first": False,
-    }
-
-elif MULTIDIM_MODEL == VectorQuantileModel.OTCP:
-    train_kwargs = {
-        "batch_size": 64,
-        "device": device,
-    }
-    multidim_params = {
-        "positive": True,
-    }
-elif MULTIDIM_MODEL == VectorQuantileModel.ENTROPIC_OT:
-    train_kwargs = {
-        "batch_size": 64,
-        "device": device,
-    }
-    multidim_params = {
-        "target": "exp",
-        "standardize": False,
-        "fit_mse_params": False,
-        "eps": 0.1,
-        "max_iters": 100,
-        "tol": 1e-6,
-        "random_state": seed,
-    }
-else:
-    raise ValueError(f"Invalid multidim model: {MULTIDIM_MODEL}")
 
 
-ENSEMBLE_GROUPS = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-]
+def main(ensemble_groups, ind_dataset, ood_dataset, uncertainty_measures, multidim_model, multidim_params, train_kwargs, weights_root, seed):
+    set_all_seeds(seed)
+    results = defaultdict(list)
 
-ind_dataset = DatasetName.CIFAR10.value
-ood_dataset = DatasetName.CIFAR100.value
+    for group in ensemble_groups:
+        all_ind_logits = []
+        all_ood_logits = []
+        for model_id in group:
+            ind_res = load_pickle(
+                f"{weights_root}/{ind_dataset}/checkpoints/resnet18/CrossEntropy/{model_id}/{ind_dataset}.pkl"
+            )
+            ood_res = load_pickle(
+                f"{weights_root}/{ind_dataset}/checkpoints/resnet18/CrossEntropy/{model_id}/{ood_dataset}.pkl"
+            )
 
-results = defaultdict(list)
+            logits_ind = ind_res["embeddings"]
+            all_ind_logits.append(ind_res["embeddings"][None])
+            all_ood_logits.append(ood_res["embeddings"][None])
 
-for group in ENSEMBLE_GROUPS:
-    all_ind_logits = []
-    all_ood_logits = []
-    for model_id in group:
-        ind_res = load_pickle(
-            f"./model_weights/{ind_dataset}/checkpoints/resnet18/CrossEntropy/{model_id}/{ind_dataset}.pkl"
-        )
-        ood_res = load_pickle(
-            f"./model_weights/{ind_dataset}/checkpoints/resnet18/CrossEntropy/{model_id}/{ood_dataset}.pkl"
+        y_ind = ind_res["labels"]
+        y_ood = ood_res["labels"]
+
+        _, train_cond_idx, calib_idx, test_idx = split_dataset_indices(
+            logits_ind,
+            y_ind,
+            train_ratio=0.0,
+            calib_ratio=0.1,
+            test_ratio=0.8,
+            random_state=seed
         )
 
-        logits_ind = ind_res["embeddings"]
-        all_ind_logits.append(ind_res["embeddings"][None])
-        all_ood_logits.append(ood_res["embeddings"][None])
+        y_train_cond = y_ind[train_cond_idx]
+        y_calib = y_ind[calib_idx]
 
-    y_ind = ind_res["labels"]
-    y_ood = ood_res["labels"]
+        X_train_cond = np.vstack(all_ind_logits)[:, train_cond_idx, :]
+        X_calib = np.vstack(all_ind_logits)[:, calib_idx, :]
+        X_test = np.vstack(all_ind_logits)[:, test_idx, :]
 
-    _, train_cond_idx, calib_idx, test_idx = split_dataset_indices(
-        logits_ind,
-        y_ind,
-        train_ratio=0.0,
-        calib_ratio=0.1,
-        test_ratio=0.8,
-        random_state=seed
+        X_ood = np.vstack(all_ood_logits)
+
+        multi_dim_uncertainty = MultiDimensionalUncertainty(
+            uncertainty_measures,
+            multidim_model=multidim_model,
+            multidim_params=multidim_params,
+            if_add_maximal_elements=True,
+        )
+        multi_dim_uncertainty.fit(
+            logits_train=X_train_cond,
+            y_train=y_train_cond,
+            logits_calib=X_calib,
+            train_kwargs=train_kwargs,
+        )
+        if multidim_model == VectorQuantileModel.CPFLOW:
+            X_test = torch.from_numpy(X_test).to(torch.float32).to(train_kwargs["device"])
+            X_ood = torch.from_numpy(X_ood).to(torch.float32).to(train_kwargs["device"])
+
+        _, uncertainty_scores_ind = multi_dim_uncertainty.predict(X_test)
+        _, uncertainty_scores_ood = multi_dim_uncertainty.predict(X_ood)
+
+        # Compute ROC AUC between in-distribution (class 0) and OOD (class 1) using sklearn
+        for k in uncertainty_scores_ind.keys():
+            ind_scores = uncertainty_scores_ind[k]
+            ood_scores = uncertainty_scores_ood[k]
+
+            # Concatenate scores and labels
+            all_scores = np.concatenate([ind_scores, ood_scores])
+            all_labels = np.concatenate(
+                [
+                    np.zeros_like(ind_scores),  # class 0: in-distribution
+                    np.ones_like(ood_scores),  # class 1: OOD
+                ]
+            )
+
+            auc = roc_auc_score(all_labels, all_scores)
+            results[k].append(auc)
+
+    print(f"train_cond_idx.shape: {train_cond_idx.shape}")
+    print(f"calib_idx.shape: {calib_idx.shape}")
+    print(f"test_idx.shape: {test_idx.shape}")
+
+    print(f"For metrics: {[m['print_name'] for m in uncertainty_measures]}")
+
+
+    rows = []
+    for metric, aucs in results.items():
+        mean_auc = np.mean(aucs)
+        std_auc = np.std(aucs)
+        row = {
+            "In-distribution": ind_dataset,
+            "Out-of-distribution": ood_dataset,
+            "Metric": metric,
+            "ROC AUC Scores": aucs,
+            "Mean ROC AUC": mean_auc,
+            "Std ROC AUC": std_auc,
+        }
+        rows.append(row)
+
+    df = pd.DataFrame(
+        rows,
+        columns=[
+            "In-distribution",
+            "Out-of-distribution",
+            "Metric",
+            "ROC AUC Scores",
+            "Mean ROC AUC",
+            "Std ROC AUC",
+        ],
     )
 
-    y_train_cond = y_ind[train_cond_idx]
-    y_calib = y_ind[calib_idx]
+    return df
 
-    X_train_cond = np.vstack(all_ind_logits)[:, train_cond_idx, :]
-    X_calib = np.vstack(all_ind_logits)[:, calib_idx, :]
-    X_test = np.vstack(all_ind_logits)[:, test_idx, :]
 
-    X_ood = np.vstack(all_ood_logits)
 
-    multi_dim_uncertainty = MultiDimensionalUncertainty(
-        UNCERTAINTY_MEASURES,
-        multidim_model=MULTIDIM_MODEL,
-        multidim_params=multidim_params,
-        if_add_maximal_elements=True,
-    )
-    multi_dim_uncertainty.fit(
-        logits_train=X_train_cond,
-        y_train=y_train_cond,
-        logits_calib=X_calib,
-        train_kwargs=train_kwargs,
-    )
+if __name__ == "__main__":
+    seed = 42
+    UNCERTAINTY_MEASURES = ADDITIVE_TOTALS # + BAYES_RISK_AND_BAYES_RISK + EXCESSES_DIFFERENT_INSTANTIATIONS
+    MULTIDIM_MODEL = VectorQuantileModel.ENTROPIC_OT
+
+    device = torch.device("cuda:0")
+
     if MULTIDIM_MODEL == VectorQuantileModel.CPFLOW:
-        X_test = torch.from_numpy(X_test).to(torch.float32).to(train_kwargs["device"])
-        X_ood = torch.from_numpy(X_ood).to(torch.float32).to(train_kwargs["device"])
+        train_kwargs = {
+            "lr": 1e-4,
+            "num_epochs": 10,
+            "batch_size": 64,
+            "device": device,
+        }
+        multidim_params = {
+            "feature_dimension": len(UNCERTAINTY_MEASURES),
+            "hidden_dim": 8,
+            "num_hidden_layers": 5,
+            "nblocks": 4,
+            "zero_softplus": False,
+            "softplus_type": "softplus",
+            "symm_act_first": False,
+        }
 
-    _, uncertainty_scores_ind = multi_dim_uncertainty.predict(X_test)
-    _, uncertainty_scores_ood = multi_dim_uncertainty.predict(X_ood)
+    elif MULTIDIM_MODEL == VectorQuantileModel.OTCP:
+        train_kwargs = {
+            "batch_size": 64,
+            "device": device,
+        }
+        multidim_params = {
+            "positive": True,
+        }
+    elif MULTIDIM_MODEL == VectorQuantileModel.ENTROPIC_OT:
+        train_kwargs = {
+            "batch_size": 64,
+            "device": device,
+        }
+        multidim_params = {
+            "target": "exp",
+            "standardize": False,
+            "fit_mse_params": False,
+            "eps": 0.1,
+            "max_iters": 100,
+            "tol": 1e-6,
+            "random_state": seed,
+        }
+    else:
+        raise ValueError(f"Invalid multidim model: {MULTIDIM_MODEL}")
 
-    # Compute ROC AUC between in-distribution (class 0) and OOD (class 1) using sklearn
-    for k in uncertainty_scores_ind.keys():
-        ind_scores = uncertainty_scores_ind[k]
-        ood_scores = uncertainty_scores_ood[k]
 
-        # Concatenate scores and labels
-        all_scores = np.concatenate([ind_scores, ood_scores])
-        all_labels = np.concatenate(
-            [
-                np.zeros_like(ind_scores),  # class 0: in-distribution
-                np.ones_like(ood_scores),  # class 1: OOD
-            ]
-        )
+    ENSEMBLE_GROUPS = [
+        [0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9],
+        [10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19],
+    ]
 
-        auc = roc_auc_score(all_labels, all_scores)
-        results[k].append(auc)
+    ind_dataset = DatasetName.CIFAR10.value
+    ood_dataset = DatasetName.CIFAR100.value
+    weights_root = "./model_weights"
 
-print(f"train_cond_idx.shape: {train_cond_idx.shape}")
-print(f"calib_idx.shape: {calib_idx.shape}")
-print(f"test_idx.shape: {test_idx.shape}")
-
-print(f"For metrics: {[m['print_name'] for m in UNCERTAINTY_MEASURES]}")
-
-
-rows = []
-for metric, aucs in results.items():
-    mean_auc = np.mean(aucs)
-    std_auc = np.std(aucs)
-    row = {
-        "In-distribution": ind_dataset,
-        "Out-of-distribution": ood_dataset,
-        "Metric": metric,
-        "ROC AUC Scores": aucs,
-        "Mean ROC AUC": mean_auc,
-        "Std ROC AUC": std_auc,
-    }
-    rows.append(row)
-
-df = pd.DataFrame(
-    rows,
-    columns=[
-        "In-distribution",
-        "Out-of-distribution",
-        "Metric",
-        "ROC AUC Scores",
-        "Mean ROC AUC",
-        "Std ROC AUC",
-    ],
-)
-
-print("\nSummary of ROC AUCs across groups:")
-print(df)
+    df = main(ENSEMBLE_GROUPS, ind_dataset, ood_dataset, UNCERTAINTY_MEASURES, MULTIDIM_MODEL, multidim_params, train_kwargs, weights_root, seed)
+    print(df)

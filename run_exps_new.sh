840e74dff3ead033ce81a59c7e1b83ebd79d9726
Nikita Kotelevskii <niket0996@gmail.com>
2025-09-11 19:15:49 +0300

    chore(cleanup): corrected typo in bash script

--- run_exps_new.sh
+++ run_exps_new.sh
@@ -1,4 +1,4 @@
-uv run python scripts/full_evaluation.py --entropic_target exp --entropic_eps 0.5 --entropic_scaling_type FeatureWise --output_file ./resources/refactored/benchmark.csv --verbose
-uv run python scripts/full_evaluation.py --entropic_target exp --entropic_eps 0.5 --entropic_scaling_type Global --output_file ./resources/refactored/benchmark.csv --verbose
-uv run python scripts/full_evaluation.py --entropic_target beta --entropic_eps 0.5 --entropic_scaling_type FeatureWise --output_file ./resources/refactored/benchmark.csv --verbose
-uv run python scripts/full_evaluation.py --entropic_target beta --entropic_eps 0.5 --entropic_scaling_type Global --output_file ./resources/refactored/benchmark.csv --verbose
+uv run python scripts/full_evaluation.py --entropic_target Exp --entropic_eps 0.5 --entropic_scaling_type FeatureWise --output_file ./resources/refactored/benchmark.csv --verbose
+uv run python scripts/full_evaluation.py --entropic_target Exp --entropic_eps 0.5 --entropic_scaling_type Global --output_file ./resources/refactored/benchmark.csv --verbose
+uv run python scripts/full_evaluation.py --entropic_target Beta --entropic_eps 0.5 --entropic_scaling_type FeatureWise --output_file ./resources/refactored/benchmark.csv --verbose
+uv run python scripts/full_evaluation.py --entropic_target Beta --entropic_eps 0.5 --entropic_scaling_type Global --output_file ./resources/refactored/benchmark.csv --verbose

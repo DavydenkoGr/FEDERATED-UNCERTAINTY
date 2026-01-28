29ff6d88bb1ae4c355455206006ad9add9e033b0
grigoriidavydenko <grigorydavydenko@yandex.ru>
2026-01-28 09:10:06 +0000

    added dependencies files

--- federated_uncertainty_scripts/run_compute_metrics.sh
+++ federated_uncertainty_scripts/run_compute_metrics.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=1 python3 ./federated_uncertainty_scripts/compute_metrics.py \
+CUDA_VISIBLE_DEVICES=0 python3 ./federated_uncertainty_scripts/compute_metrics.py \
     --n_clients 5 \
     --data_path "/home/grigoriidavydenko/workplace/FEDERATED-UNCERTAINTY/data/saved_models/cifar100/test_run5"
